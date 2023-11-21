806bc8bc68575f2eaac8ca4538e10ac1cd22459b
大谷周平 <yellow.monkey.type.f@gmail.com>
2023-11-22 00:15:02 +0900

    初回コミット
    JAVA版Dumbo開発開始

---
+++ src/main/java/bdcp/dumbo/DumboApplication.java
@@ -0,0 +1,13 @@
+package bdcp.dumbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DumboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bdcp/dumbo/DumboApplicationTests.java
@@ -0,0 +1,13 @@
+package bdcp.dumbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DumboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
